e21493b38c7b5ea7c1a5cbf222093f7bffd780b5
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-09-10 16:58:56 -0300

    Change antMatchers WebSecurityConfig

--- src/main/java/com/alura/challenge/raphaelf/aluraflix/config/WebSecurityConfig.java
+++ src/main/java/com/alura/challenge/raphaelf/aluraflix/config/WebSecurityConfig.java
@@ -26,8 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/videos/free","/","/h2-console/**","/login/**").permitAll()
-                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers("/","/videos/free","/h2-console/**","/login/**","/swagger-ui/**").permitAll()
                 .anyRequest().authenticated()
                 .and().cors()
                 .and().headers().frameOptions().sameOrigin()
